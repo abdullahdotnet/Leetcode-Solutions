f17d15aeff18f25108fdc6166e56d6c355454feb
abdullah <abdullahdotnet20@gmail.com>
2024-12-26 14:26:59 +0500

    1 problem

---
+++ String  & Array/189-RotateArray.py
@@ -0,0 +1,33 @@
+# def rotateArray(nums,k):
+#     for i in range(k):
+#         nums.insert(0,nums[-1])
+#         nums.pop()
+#     return nums
+
+
+# def rotateArray(nums,k):
+#     n = len(nums)
+#     result = [0]*n
+#     k = k%n
+#     for i in range(len(nums)):
+#         result[(i+k)%n] = nums[i]
+#     for i in range(len(nums)):
+#         nums[i] = result[i]
+
+# def rotateArray(nums,k):
+#     k = k%len(nums)
+#     if k!=0:
+#         nums[:k],nums[k:] = nums[-k:],nums[:-k]
+#     return nums
+
+def rotateArray(nums,k):
+    k%=len(nums)
+    def reverse(left,right):
+        while left < right:
+            nums[left],nums[right] = nums[right],nums[left]
+            left+=1
+            right-=1
+    reverse(0,len(nums)-1)
+    reverse(0,k-1)
+    reverse(k,len(nums)-1)
+print(rotateArray([-1,-100,3,99],2))
