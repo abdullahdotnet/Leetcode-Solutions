28d1898138ebea9bce8a99fedd3720b2fe47c601
abdullah <abdullahdotnet20@gmail.com>
2024-12-31 20:43:33 +0500

    another solution for previous problem

--- String  & Array/151-ReverseWordsInAString.py
+++ String  & Array/151-ReverseWordsInAString.py
@@ -3,3 +3,15 @@ def reverseWords(s):
     s = s.split()
     res = " ".join(s[::-1])
     return res
+
+# def reverseWords(s):
+#     words = s.split()
+#     left, right = 0, len(words) - 1
+
+#     while left < right:
+#         words[left], words[right] = words[right], words[left]
+#         left += 1
+#         right -= 1
+
+#     return " ".join(words) 
+
